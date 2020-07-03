c1e9b84aa7f972682a2ba6ac3d4833bcf2444459
Daniel Cole <danielscole1@gmail.com>
2020-07-03 18:32:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
 
-<<<<<<< HEAD
   final_hash = data.each_with_object({}) do |(key, values), final_array|
-=======
-  final_hash = data.map {|key, values| final_array }.to_h
->>>>>>> 8d46536fd94b6181a45e3143688dfbd3320bf434
     values.each do |key2,names|
       names.each do |single_name|
         if !final_array[single_name]
